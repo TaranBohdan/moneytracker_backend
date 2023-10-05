1c4c094b46e17b5bc60135da0f72b607accc840f
BohdanTaran <cronbodya@gmail.com>
2023-10-05 13:13:24 +0200

    solved problec with encoding password

--- src/main/java/org/bohdan/moneytracker/services/UserService.java
+++ src/main/java/org/bohdan/moneytracker/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class UserService implements UserDetailsService
 {
     private final UserRepository userRepository;
     private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
     public Optional<User> findByUsername(String username)
     {
@@ -48,7 +50,7 @@ public class UserService implements UserDetailsService
         User user = new User();
 
         user.setUsername(signupDto.getUsername());
-        user.setPassword(signupDto.getPassword());
+        user.setPassword(passwordEncoder.encode(signupDto.getPassword()));
         user.setRoles(List.of(roleService.getRole()));
 
         return userRepository.save(user);
